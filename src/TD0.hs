466eeff2218504e974974daea15bae8727537f24
Lisa Vasilenko <vasilliza@gmail.com>
2022-02-03 17:38:54 +0100

    foldM -> mapM

--- src/TD0.hs
+++ src/TD0.hs
@@ -14,6 +14,7 @@ import           Prelude                 hiding ( map
                                                 , fmap
                                                 , mapM
                                                 , iterate
+                                                , uncurry
                                                 )
 
 
@@ -43,11 +44,25 @@ lq_required _ = ()
 td0 :: Int -> Transition -> ValueFunction -> DistrValueFunction
 td0 n t v = iterate n (act t) v
 
-{-@ measure TD0.foldM :: (b -> a -> Distr b) -> b -> List a -> Distr b @-}
-{-@ assume foldM :: f:(b -> a -> Distr b) -> z:b -> xs:List a -> {v:Distr b|v == TD0.foldM f z xs} @-}
-foldM :: (b -> a -> Distr b) -> b -> List a -> Distr b
-foldM _ z Nil = ppure z
-foldM _ _ _ = undefined
+{-@ measure TD0.foldM :: (b -> a -> Distr b) -> List a -> b -> Distr b @-}
+{-@ assume foldM :: f:(b -> a -> Distr b) -> xs:List a -> z:b -> {v:Distr b|v == TD0.foldM f xs z} @-}
+foldM :: (b -> a -> Distr b) -> List a -> b -> Distr b
+foldM _ Nil z = ppure z
+foldM f (Cons x xs) z = bind (f z x) (foldM f xs)
+
+{-@ reflect cons @-}
+cons :: Distr (List a) -> a -> Distr (List a)
+cons xs x = bind xs (ppure `o` (Cons x))
+
+{-@ reflect purecons @-}
+purecons :: a -> List a -> Distr (List a)
+purecons x xs = ppure (Cons x xs)
+
+{-@ measure TD0.mapM :: (a -> Distr b) -> List a -> Distr (List b) @-}
+{-@ assume mapM :: f:_ -> xs:_ -> {v:Distr (List b)|v == TD0.mapM f xs} @-}
+mapM :: (a -> Distr b) -> List a -> Distr (List b)
+mapM _ Nil = ppure Nil
+mapM f (Cons x xs) = bind (f x) (cons (mapM f xs))
 
 -- flipConst :: (b -> Distr b) -> b -> a -> Distr b
 
@@ -57,17 +72,21 @@ iterate :: Int -> (b -> Distr b) -> b -> Distr b
 iterate n _ x | n <= 0 = ppure x
 iterate n f x = bind (f x) (iterate (n - 1) f)
 
+{-@ reflect o @-}
+o :: (b -> c) -> (a -> b) -> a -> c
+o g f x = g (f x)
+
 {-@ reflect act @-}
 act :: Transition -> ValueFunction -> DistrValueFunction
-act t v = foldM (sample t v) Nil (range 0 (llen v))
+act t v = mapM (sample t v) (range 0 (llen v)) 
+
+{-@ reflect uncurry @-}
+uncurry :: (a -> b -> c) -> (a, b) -> c
+uncurry f (a, b) = f a b
 
 {-@ reflect sample @-}
-sample :: Transition -> ValueFunction -> ValueFunction -> State -> DistrValueFunction
-sample t v w i = bind (t `at` i) (cons (update v i) w)
-
-{-@ reflect cons @-}
-cons :: (State -> Reward -> Reward) -> ValueFunction -> (State, Reward) -> DistrValueFunction
-cons u w (i, r) = ppure (Cons (u i r) w)
+sample :: Transition -> ValueFunction -> State -> Distr Reward
+sample t v i = bind (t `at` i) (ppure `o` (uncurry (update v i)))
 
 {-@ reflect γ @-}
 {-@ reflect α @-}
--- src/TD0Theorem.hs
+++ src/TD0Theorem.hs
@@ -30,23 +30,25 @@ relationaltd0 n t v1 v2
     = relationaliterate (distList v1 v2) k n (act t) (relationalact t) v1 v2 
 
 {-@ relationaliterate :: m:_ -> {k:_|k >= 0} -> n:Nat -> f:_ -> 
-                            (m:_ -> x1:_ -> {x2:_|distList x1 x2 <= m} -> {lift (bounded (k * m)) (f x1) (f x2)}) ->
-                            x1:_ -> {x2:_|distList x1 x2 <= m} -> 
-                            {lift (bounded (pow k n * m)) (iterate n f x1) (iterate n f x2)} @-}
+                            (m:_ -> x1:_ -> x2:_ -> {bounded m x1 x2 => lift (bounded (k * m)) (f x1) (f x2)}) ->
+                            x1:_ -> x2:_ -> 
+                            {bounded m x1 x2 => lift (bounded (pow k n * m)) (iterate n f x1) (iterate n f x2)} @-}
 relationaliterate :: Double -> Double -> Int -> (List Double -> Distr (List Double)) ->
                         (Double -> List Double -> List Double -> ()) -> List Double -> List Double -> ()
 relationaliterate m k n _ _ x1 x2 | n <= 0
-    =   liftPure (bounded (pow k n * m)) x1 x2  
+    =   undefined
+        -- liftPure (bounded (pow k n * m)) x1 x2  
             -- ? (distList x1 x2 
             --     =<= m
             --     =<= pow k n * m
             --     *** QED)
 relationaliterate m k n f lemma x1 x2
-    =   liftBind (bounded (pow k n * m)) (bounded (k * m)) 
-                 (f x1) (iterate (n - 1) f)
-                 (f x2) (iterate (n - 1) f)
-                 (relationaliterate (k * m) k (n - 1) f lemma)
-            ? lemma m x1 x2
+    =   undefined
+        -- liftBind (bounded (pow k n * m)) (bounded (k * m)) 
+        --          (f x1) (iterate (n - 1) f)
+        --          (f x2) (iterate (n - 1) f)
+        --          (relationaliterate (k * m) k (n - 1) f lemma)
+        --     ? lemma m x1 x2
             
 
 -- {-@ relationaliterate :: n:Nat -> {k:_|k > 0} -> f:_ -> v1:_ -> v2:_ -> 
@@ -77,11 +79,46 @@ shrinks t k v1' v2' = dist (act t v1') (act t v2') <= k * dist v1' v2'
 bounded :: Double -> ValueFunction -> ValueFunction -> Bool
 bounded m v1' v2' = distList v1' v2' <= m
 
+{-@ reflect bounded' @-}
+bounded' :: Double -> Double -> Double -> Bool
+bounded' m x1 x2 = dist x1 x2 <= m
+
+-- consM :: 
+-- consM = undefined
+
+-- {-@ consbounded :: m:Double -> x1:Distr Double -> xs1:Distr (List Double) -> {x2:Distr Double|bounded' m x1 x2} -> {xs2:Distr (List Double)|bounded m xs1 xs2} -> 
+--                     {bounded m (consM x1 xs1) (consM x2 xs2)} @-}
+-- consbounded :: Double -> Distr Double -> Distr (List Double) -> Distr Double -> Distr (List Double) -> ()
+-- consbounded = undefined
+
+{-@ relationalmapM :: m:_ -> k:_ -> f1:_ -> v1:_ -> f2:_ -> v2:_ -> 
+                        (x1:_ -> {x2:_|dist x1 x2 <= m} -> {lift (bounded' (k * m)) (f1 x1) (f2 x2)}) ->
+                        {lift (bounded (k * m)) (mapM f1 v1) (mapM f2 v2)} @-}
+relationalmapM :: Double -> Double -> (Double -> Distr Double) -> List Double -> (Double -> Distr Double) -> List Double -> 
+                    (Double -> Double -> ()) -> ()
+-- mapM _ Nil = ppure Nil
+relationalmapM m k _ v1 _ Nil _
+    =   liftPure (bounded (k * m)) v1 Nil
+relationalmapM m k _ Nil _ v2 _
+    =   liftPure (bounded (k * m)) Nil v2
+-- mapM f (Cons x xs) = bind (f x) (cons (mapM f xs))
+relationalmapM m k f1 v1 f2 (Cons i is) lemma = undefined
+    -- =   liftBind (bounded (k * m)) trueP
+    --         (f1 i1) (cons (mapM f is2))
+    --         (\i1 i2 -> 
+    --             distList (cons (mapM f xs1) x1) (cons (mapM f xs2) x2)) 
+    --             =<= k * m
+    --             *** QED)
+
 {-@ relationalact :: t:_ -> m:_ -> v1:_ -> v2:_ -> 
                     {bounded m v1 v2 => lift (bounded (k * m)) (act t v1) (act t v2)} @-}
 relationalact :: Transition -> Double -> ValueFunction -> ValueFunction -> ()
 relationalact t m v1 v2 
-    =   undefined   
+    =   undefined
+    -- relationalmapM m k 
+    --         (sample t v1) (range 0 (llen v1)) 
+    --         (sample t v2) (range 0 (llen v2)) 
+    --         (relationalsample m t v1 v2)
 
 -- {-@ reflect shrinks @-}
 -- shrinks :: Double -> List (Distr a -> Distr b) -> List (Distr a -> Distr b) -> Distr a -> Distr a -> Bool
@@ -96,19 +133,19 @@ relationalact t m v1 v2
 -- relationalap :: List (a -> b) -> List a -> List (a -> b) -> List a -> (a -> a -> ()) -> ()
 -- relationalap xs1 xs2 f = undefined
 
-{-@ relationalsample :: m:_ -> t:_ -> v1:_ -> w1:_ -> v2:_ -> w2:_ -> i:_ -> 
-                        {bounded m v1 v2 && bounded (k * m) w1 w2 => lift (bounded (k * m)) (sample t v1 w1 i) (sample t v2 w2 i)} @-}
-relationalsample :: Double -> Transition -> ValueFunction -> ValueFunction -> ValueFunction -> ValueFunction -> State -> ()
+{-@ relationalsample :: m:_ -> t:_ -> i:_ -> v1:_ -> v2:_ ->  
+                        {bounded m v1 v2 => lift (bounded' (k * m)) (sample t v1 i) (sample t v2 i)} @-}
+relationalsample :: Double -> Transition -> State -> ValueFunction -> ValueFunction -> ()
 relationalsample = undefined
 
 -- maybe bounded m*k w1 w2 => lift (bounded m*k) ...
-{-@ relationalcons :: m:_ -> u1:_ -> w1:_ -> u2:_ -> w2:_ -> p:(j:_, r:_) ->
-                        {dist (u1 (fst p) (snd p)) (u2 (fst p) (snd p)) <= m && bounded m w1 w2 => 
-                            lift (bounded m) (cons u1 w1 p) (cons u2 w2 p)} @-}
-relationalcons :: Double -> (State -> Reward -> Reward) -> ValueFunction -> 
-                            (State -> Reward -> Reward) -> ValueFunction -> 
-                            (State, Reward) -> ()
-relationalcons = undefined
+-- {-@ relationalcons :: m:_ -> u1:_ -> w1:_ -> u2:_ -> w2:_ -> p:(j:_, r:_) ->
+--                         {dist (u1 (fst p) (snd p)) (u2 (fst p) (snd p)) <= m && bounded m w1 w2 => 
+--                             lift (bounded m) (cons u1 w1 p) (cons u2 w2 p)} @-}
+-- relationalcons :: Double -> (State -> Reward -> Reward) -> ValueFunction -> 
+--                             (State -> Reward -> Reward) -> ValueFunction -> 
+--                             (State, Reward) -> ()
+-- relationalcons = undefined
 
 {-@ relationalupdate :: m:_ -> v1:_ -> v2:_ -> i:_ -> j:_ -> r:_ ->
                         {dist (update v1 i j r) (update v2 i j r) 
