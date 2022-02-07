fe71ae24c4b659508af188bb6c9cc4f62189b8ac
Lisa Vasilenko <vasilliza@gmail.com>
2022-02-07 14:59:42 +0100

    proe lemmas in Sample

--- src/Lemmata/Relational/Sample.hs
+++ src/Lemmata/Relational/Sample.hs
@@ -14,23 +14,41 @@ import           Lemmata.Relational.Update
 import           TD0 
 import           Language.Haskell.Liquid.ProofCombinators
 
+-- TODO: listLemma will be unnecessary when `at` is reflected
+{-@ listLemma :: v1:_ -> v2:_ -> i:_ -> {dist (at v1 i) (at v2 i) <= distList v1 v2} @-}
+listLemma :: ValueFunction -> ValueFunction -> State -> ()
+listLemma = undefined
 
-{-@ lemma :: m:_ -> v1:_ -> {v2:_|bounded m v1 v2} -> i:_ -> t1:_ -> {t2:_|eqP t1 t2} -> 
+{-@ maxLemma :: v1:_ -> v2:_ -> i:_ -> j:_ -> 
+                    {max (dist (at v1 i) (at v2 i)) (dist (at v1 j) (at v2 j)) <= distList v1 v2 } @-}
+maxLemma :: ValueFunction -> ValueFunction -> State -> State -> ()
+maxLemma v1 v2 i j 
+    =   max (dist (at v1 i) (at v2 i)) (dist (at v1 j) (at v2 j)) 
+        ? listLemma v1 v2 i
+    =<= max (distList v1 v2) (dist (at v1 j) (at v2 j))
+        ? listLemma v1 v2 j
+    =<= max (distList v1 v2) (distList v1 v2)
+    =<= distList v1 v2
+    *** QED
+
+{-@ updateLemma :: v1:_ -> v2:_ -> i:_ -> j:_ -> r:_ -> 
+                    {dist (update v1 i j r) (update v2 i j r) <= k * distList v1 v2} @-}
+updateLemma :: ValueFunction -> ValueFunction -> State -> State -> Reward -> ()
+updateLemma v1 v2 i j r
+    =   dist (update v1 i j r) (update v2 i j r)
+        ? relationalupdate v1 v2 i j r
+    =<= k * max (dist (at v1 i) (at v2 i)) (dist (at v1 j) (at v2 j))
+        ? maxLemma v1 v2 i j
+    =<= k * distList v1 v2
+    *** QED
+
+{-@ pureUpdateLemma :: m:_ -> v1:_ -> {v2:_|bounded m v1 v2} -> i:_ -> t1:_ -> {t2:_|eqP t1 t2} -> 
                 {lift (bounded' (m * k)) (ppure (uncurry (update v1 i) t1)) (ppure (uncurry (update v2 i) t2))} @-}
-lemma :: Double -> ValueFunction -> ValueFunction -> State -> (State, Reward) -> (State, Reward) -> ()
-lemma m v1 v2 i (j1, r1) (j2, r2) = 
+pureUpdateLemma :: Double -> ValueFunction -> ValueFunction -> State -> (State, Reward) -> (State, Reward) -> ()
+pureUpdateLemma m v1 v2 i (j1, r1) (j2, r2) = 
     liftPure (bounded' (max (dist (at v1 i) (at v2 i)) (dist (at v1 j1) (at v2 j2))))
                 (update v1 i j1 r1) (update v2 i j2 r2) 
-                (dist (update v1 i j1 r1) (update v2 i j2 r2)
-                    === dist (update v1 i j1 r1) (update v2 i j1 r1)
-                        ? relationalupdate v1 v2 i j1 r1
-                    =<= k * max (dist (at v1 i) (at v2 i)) (dist (at v1 j1) (at v2 j1))
-                        ? (max (dist (at v1 i) (at v2 i)) (dist (at v1 j1) (at v2 j2))
-                            =<= distList v1 v2 
-                            =<= m
-                            *** QED)
-                    =<= k * m
-                        *** QED)
+                (updateLemma v1 v2 i j1 r1)
 
 {-@ relationalsample :: m:_ -> t:_ -> v1:_ -> v2:_ -> i:_ ->  
                         {bounded m v1 v2 => lift (bounded' (k * m)) (sample t v1 i) (sample t v2 i)} @-}
@@ -40,5 +58,5 @@ relationalsample m t v1 v2 i | bounded m v1 v2
                (t `at` i) (ppure `o` (uncurry (update v1 i)))
                (t `at` i) (ppure `o` (uncurry (update v2 i)))
                (liftEq (t `at` i))
-               (lemma m v1 v2 i)
+               (pureUpdateLemma m v1 v2 i)
 relationalsample _ _ _ _ _ = ()
