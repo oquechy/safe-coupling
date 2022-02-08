b1be04232f6239636636663841ab9823293c2e02
Lisa Vasilenko <vasilliza@gmail.com>
2022-02-08 15:12:11 +0100

    prove Sample

--- src/Lemmata/Relational/Sample.hs
+++ src/Lemmata/Relational/Sample.hs
@@ -14,10 +14,12 @@ import           TD0
 import           Language.Haskell.Liquid.ProofCombinators
 import           Misc.ProofCombinators
 
--- TODO: listLemma will be unnecessary when `at` is reflected
-{-@ listLemma :: v1:_ -> v2:_ -> i:_ -> {dist (at v1 i) (at v2 i) <= distList v1 v2} @-}
+{-@ listLemma :: v1:_ -> v2:SameLen v1 -> i:StateOf v1 -> {dist (at v1 i) (at v2 i) <= distList v1 v2} @-}
 listLemma :: ValueFunction -> ValueFunction -> State -> ()
-listLemma = undefined
+listLemma Nil v2 i = ()
+listLemma v1 Nil i = ()
+listLemma (Cons x xs) (Cons y ys) 0 = ()
+listLemma (Cons x xs) (Cons y ys) i = listLemma xs ys (i - 1)
 
 {-@ maxLemma :: v1:_ -> v2:SameLen v1 -> i:StateOf v1 -> j:StateOf v1 -> 
                     {max (dist (at v1 i) (at v2 i)) (dist (at v1 j) (at v2 j)) <= distList v1 v2 } @-}
@@ -42,19 +44,36 @@ updateLemma v1 v2 i j r
     =<= k * distList v1 v2
     *** QED
 
-{-@ ignore pureUpdateLemma @-}
-{-@ pureUpdateLemma :: m:_ -> v1:_ -> {v2:_|bounded m v1 v2 && llen v1 = llen v2} -> i:StateOf v1 -> t1:(StateOf v1, Reward) -> {t2:(StateOf v1, Reward)|eqP t1 t2} -> 
-                {lift (bounded' (m * k)) (ppure (uncurry (update v1 i) t1)) (ppure (uncurry (update v2 i) t2))} @-}
-pureUpdateLemma :: Double -> ValueFunction -> ValueFunction -> State -> (State, Reward) -> (State, Reward) -> ()
-pureUpdateLemma m v1 v2 i (j1, r1) (j2, r2) = 
-    liftPure (bounded' (max (dist (at v1 i) (at v2 i)) (dist (at v1 j1) (at v2 j2))))
-                (update v1 i j1 r1) (update v2 i j2 r2) 
-                (updateLemma v1 v2 i j1 r1)
+{-@ uncurryLemma :: {m:_|0 <= m} -> v1:_ -> v2:SameLen v1 -> {_:_|bounded m v1 v2} -> i:StateOf v1 
+                 -> t1:(StateOf v1, Reward) -> {t2:(StateOf v1, Reward)|t1 = t2}
+                 -> {bounded' (k * m) (uncurry (update v1 i) t1) (uncurry (update v2 i) t2)} @-}
+uncurryLemma :: Double -> ValueFunction -> ValueFunction -> () -> State 
+             -> (State, Reward) -> (State, Reward) 
+             -> ()
+uncurryLemma m v1 v2 b i t1@(j1, r1) t2@(j2, r2) 
+    =   dist (uncurry (update v1 i) t1) (uncurry (update v2 i) t2)
+    === dist (uncurry (update v1 i) t1) (uncurry (update v2 i) t1)
+    === dist (update v1 i j1 r1) (update v2 i j1 r1)
+        ? updateLemma v1 v2 i j1 r1
+    =<= k * distList v1 v2
+        ? b
+    =<= k * m
+    *** QED
+
+{-@ pureUpdateLemma :: {m:_|0 <= m} -> v1:_ -> v2:SameLen v1 -> {_:_|bounded m v1 v2} -> i:StateOf v1 
+                    -> t1:(StateOf v1, Reward) -> {t2:(StateOf v1, Reward)|eqP t1 t2} 
+                    -> {lift (bounded' (m * k)) (ppure (uncurry (update v1 i) t1)) (ppure (uncurry (update v2 i) t2))} @-}
+pureUpdateLemma :: Double -> ValueFunction -> ValueFunction -> () -> State -> (State, Reward) -> (State, Reward) -> ()
+pureUpdateLemma m v1 v2 b i t1@(j1, r1) t2@(j2, r2) = 
+    liftPure (bounded' (k * m))
+                (uncurry (update v1 i) t1) (uncurry (update v2 i) t2)
+                (uncurryLemma m v1 v2 b i t1 t2)
+        
 
 {-@ ignore relationalsample@-}
 -- INDEXING NV SHOULD FIX 
 
-{-@ relationalsample :: m:_ -> t:_ -> {v1:_|llen t = llen v1} -> {v2:_|llen t = llen v2} -> i:StateOf t ->  
+{-@ relationalsample :: {m:_|0 <= m} -> t:_ -> {v1:_|llen t = llen v1} -> {v2:_|llen t = llen v2} -> i:StateOf t ->  
                         {bounded m v1 v2 => lift (bounded' (k * m)) (sample v1 t i) (sample v2 t i)} @-}
 relationalsample :: Double -> Transition -> ValueFunction -> ValueFunction -> State -> ()
 relationalsample m t v1 v2 i | bounded m v1 v2 
@@ -62,5 +81,5 @@ relationalsample m t v1 v2 i | bounded m v1 v2
                (t `at` i) (ppure `o` (uncurry (update v1 i)))
                (t `at` i) (ppure `o` (uncurry (update v2 i)))
                (liftEq (t `at` i))
-               (pureUpdateLemma m v1 v2 i)
+               (pureUpdateLemma m v1 v2 () i)
 relationalsample _ _ _ _ _ = ()
