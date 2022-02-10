c098cd470a2f646dd21b833e7b3c729d28c73974
Niki Vazou <nikivazou@gmail.com>
2022-02-10 12:18:02 +0100

    all good, again

--- src/Lemmata/Relational/Iterate.hs
+++ src/Lemmata/Relational/Iterate.hs
@@ -10,13 +10,15 @@ import           Prelude hiding (iterate)
 
 import           TD0 
 import           Language.Haskell.Liquid.ProofCombinators
+import           Misc.ProofCombinators
 
 
 {-@ relationaliterate :: m:{_|0 <= m} -> {k:_|k >= 0} -> n:Nat -> l:Nat
                       -> f:(v:ListN l -> Distr (ListN l))
                       -> (m:{_|0 <= m} -> y1:{List Double|llen y1 = l} -> y2:{List Double|llen y2 = l} -> {bounded m y1 y2 => lift (bounded (k * m)) (f y1) (f y2)})
                       -> x1:ListN l -> x2:ListN l
-                      -> {bounded m x1 x2 => lift (bounded (pow k n * m)) (iterate n (llen x1) f x1) (iterate n (llen x2) f x2)} / [n] @-}
+                      -> {bounded m x1 x2 => lift (bounded (pow k n * m)) ((iterate n (llen x1) f) (x1)) 
+                                                                          ((iterate n (llen x2) f) (x2))} / [n] @-}
 relationaliterate :: Double -> Double -> Int -> Int
                   -> (List Double -> Distr (List Double)) 
                   -> (Double -> List Double -> List Double -> ()) 
@@ -25,7 +27,8 @@ relationaliterate :: Double -> Double -> Int -> Int
 relationaliterate m k 0 _ _ _ x1 x2 | bounded m x1 x2
     =   liftPure (bounded (pow k 0 * m)) x1 x2 ()
 relationaliterate m k n l f lemma x1 x2 | bounded m x1 x2
-    =   liftBind (bounded (pow k n * m)) (bounded (k * m)) 
+    =   assert (pow k (n-1) * (k * m) == pow k n * m) ? 
+        liftBind (bounded (pow k n * m)) (bounded (k * m)) 
                  (f x1) (iterate (n - 1) (llen x1) f)
                  (f x2) (iterate (n - 1) (llen x2) f)
                  (lemma m x1 x2)
