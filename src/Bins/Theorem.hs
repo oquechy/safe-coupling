b86d6f4ada52fb7a9481badc61f960ff93103cac
Niki Vazou <nikivazou@gmail.com>
2022-02-25 17:35:12 +0100

    done with bins

--- src/Bins/Theorem.hs
+++ src/Bins/Theorem.hs
@@ -12,6 +12,7 @@ import           Prelude                 hiding ( flip )
 import           Monad.Distr.Predicates
 import           Monad.Distr.Relational.TCB.Spec 
 import           Monad.Distr.Relational.TCB.EDist
+import           Monad.Distr.Relational.Theorems
 import           Bins.Bins
 
 import           Language.Haskell.Liquid.ProofCombinators
@@ -50,7 +51,8 @@ binsSpec p q n
 
 -}
 
-{-@ plusDist :: y:Double -> x1:Double -> x2:Double -> {distD (plus y x1) (plus y x2) = distD x1 x2} @-}
+{-@ plusDist :: y:Double -> x1:Double -> x2:Double 
+             -> {distD (plus y x1) (plus y x2) = distD x1 x2} @-}
 plusDist :: Double -> Double -> Double -> ()
 plusDist _ _ _ = ()
 
@@ -65,7 +67,11 @@ addBernoulliDist p q n y
                  (plus y) (bernoulli q)
                  (plusDist y)
   =<= dist (kant distDouble) (bernoulli p) (bernoulli q)
-        ? (bernoulliDist distDouble p q)
+     ? (bernoulliDist distDouble p q)
+     ? assert (distD 1.0 0.0 == 1.0)
+     ? assert (distD 1 0 * (q-p) == q-p)
+  =<= distD 1 0 * (q -p)
+  =<=  q - p
   *** QED
 
 {-@ binsDistL :: p:Prob -> {q:Prob|p <= q} -> {n:PDouble|1 <= n}
@@ -82,8 +88,8 @@ binsDistL p q n
                 -> {dist (kant distDouble) 
                          (seqBind (bins p n) (flip (pure2 plus)) x)
                          (seqBind (bins q n) (flip (pure2 plus)) x)
-                          <= n * (q - p)} @-}
-addBinsDist :: Prob -> Prob -> Double -> Double -> ()
+                          <= n * (q - p)} / [n, 2] @-}
+addBinsDist ::  Prob -> Prob -> Double -> Double -> ()
 addBinsDist p q n x 
   =   dist (kant distDouble) 
            (seqBind (bins p n) (flip (pure2 plus)) x)
@@ -91,63 +97,101 @@ addBinsDist p q n x
   === dist (kant distDouble) 
            (bind (bins p n) (flip (pure2 plus) x))
            (bind (bins q n) (flip (pure2 plus) x))
+      ? flipPlus x 
   === dist (kant distDouble) 
            (bind (bins p n) (ppure . (plus x)))
            (bind (bins q n) (ppure . (plus x)))
         ? pureBindDist distDouble distDouble
-                       (n * (q - p))
+                       0
                        (plus x) (bins p n)
                        (plus x) (bins q n)
                        (plusDist x)
-  =<= n * (q - p)
+  =<= dist (kant distDouble) (bins p n) (bins q n)
+       ? binsDist p q n 
+  =<=  n * (q - p) 
   *** QED
 
 {-@ reflect pure2 @-}
 pure2 :: (a -> b -> c) -> a -> b -> Distr c
 pure2 f a b = ppure (f a b)
 
+
+{-@ addBernoulliEq :: n:{Double | 0 <= n - 1 } -> p:Prob -> q:Prob 
+                   -> {addBernoulli q (n - 1) == seqBind (bernoulli q) (pure2 plus)} @-}
+addBernoulliEq :: Double -> Double -> Double -> () 
+addBernoulliEq n p q 
+  = extDouble (addBernoulli q (n - 1)) (seqBind (bernoulli q) (pure2 plus)) 
+              (addBernoulliEq' n p q)
+
+{-@ addBernoulliEq' :: n:{Double | 0 <= n - 1 } -> p:Prob -> q:Prob 
+                    -> x:{Double | 0 <= x && x <= n - 1 }
+                   -> {addBernoulli q (n - 1) x == seqBind (bernoulli q) (pure2 plus) x} @-}
+addBernoulliEq' :: Double -> Double -> Double -> Double -> () 
+addBernoulliEq' n p q x
+  =   addBernoulli q (n - 1) x 
+  === bind (bernoulli q) (ppure . plus x)
+       ? extDouble (ppure . plus x) (pure2 plus x) (
+           \z -> (ppure . plus x) z  === pure2 plus x z *** QED 
+       )
+  === bind (bernoulli q) (pure2 plus x)
+  === bind (bernoulli q) (pure2 plus x)
+  === seqBind (bernoulli q) (pure2 plus) x
+  *** QED 
+
+
+
 {-@ binsDistR :: p:Prob -> {q:Prob|p <= q} -> {n:PDouble|1 <= n} 
-             -> {dist (kant distDouble) (bins' p q n) (bins q n) <= (n - 1) * (q - p)} @-}
-binsDistR :: Prob -> Prob -> Double -> ()
+              -> {dist (kant distDouble) (bins' p q n) (bins q n) <= (n - 1) * (q - p)} 
+              / [n, 0] @-}
+binsDistR ::Prob -> Prob -> Double -> ()
 binsDistR p q n 
-  =   dist (kant distDouble) (bins' p q n) (bins q n)
-      ? assume (bins' p q n == bind (bins p (n - 1)) (seqBind (bernoulli q) (pure2 plus)))
-      ? assume (bins q n == bind (bins q (n - 1)) (seqBind (bernoulli q) (pure2 plus)))
-  === dist (kant distDouble) 
+  =   dist (kant d) (bins' p q n) (bins q n)
+      ? addBernoulliEq n p q 
+      ? assert (bins' p q n == bind (bins p (n - 1)) (seqBind (bernoulli q) (pure2 plus)))
+      ? assert (bins q n == bind (bins q (n - 1)) (seqBind (bernoulli q) (pure2 plus)))
+  === dist (kant d) 
            (bind (bins p (n - 1)) (seqBind (bernoulli q) (pure2 plus))) 
            (bind (bins q (n - 1)) (seqBind (bernoulli q) (pure2 plus)))
       ? commutative (bins p (n - 1)) (bernoulli q) (pure2 plus)
       ? commutative (bins q (n - 1)) (bernoulli q) (pure2 plus)
-  === dist (kant distDouble) 
+  === dist (kant d) 
            (bind (bernoulli q) (seqBind (bins p (n - 1)) (flip (pure2 plus)))) 
            (bind (bernoulli q) (seqBind (bins q (n - 1)) (flip (pure2 plus))))
-        ? bindDistEq distDouble
+        ? bindDistEq d
                      ((n - 1) * (q - p))
                      (seqBind (bins p (n - 1)) (flip (pure2 plus))) (bernoulli q)
                      (seqBind (bins q (n - 1)) (flip (pure2 plus))) (bernoulli q)
                      (addBinsDist p q (n - 1))
   =<= (n - 1) * (q - p)
   *** QED
+    where d = distDouble 
 
 {-@ binsDist :: p:Prob -> {q:Prob|p <= q} -> n:PDouble 
-             -> {dist (kant distDouble) (bins p n) (bins q n) <= n * (q - p)} @-}
+             -> {dist (kant distDouble) (bins p n) (bins q n) <= n * (q - p)} 
+             / [n, 1] @-}
 binsDist :: Prob -> Prob -> Double -> ()
-binsDist p q n | n < 1 = pureDist distDouble 0 0
+binsDist p q n | n < 1.0 
+  = pureDist distDouble 0 0 
+--   ? assert (dist (kant distDouble) (pure 0.0) (pure 0.0) == 0.0) 
+  ? assert (0 <= n) 
+  ? assert (0 <= (q - p)) 
+  ? assert (dist (kant distDouble) (bins p n) (bins q n) <= n * (q - p)) 
 binsDist p q n
-  =   dist (kant distDouble) (bins p n) (bins q n)
-      ? triangularIneq (kant distDouble) (bins p n) (bins' q p n) (bins q n)
-      ? assume (dist (kant distDouble) (bins p n) (bins q n) 
-                  <= dist (kant distDouble) (bins p n) (bins' p q n)
-                      + dist (kant distDouble) (bins' p q n) (bins q n))
-  =<= dist (kant distDouble) (bins p n) (bins' p q n)
-        + dist (kant distDouble) (bins' p q n) (bins q n)  
+  =   dist (kant d) (bins p n) (bins q n)
+      ? triangularIneq (kant d) (bins p n) (bins' p q n) (bins q n)
+      ? assert (dist (kant d) (bins p n) (bins q n) 
+                   <= dist (kant d) (bins p n) (bins' p q n)
+                    + dist (kant d) (bins' p q n) (bins q n))
+  =<= dist (kant d) (bins p n) (bins' p q n)
+        + dist (kant d) (bins' p q n) (bins q n)  
       ? binsDistL p q n
   =<= q - p
-        + dist (kant distDouble) (bins' p q n) (bins q n)  
+        + dist (kant d) (bins' p q n) (bins q n)  
       ? binsDistR p q n
   =<= q - p + (n - 1) * (q - p)
-  === n * (q - p)
+  =<= n * (q - p)
   *** QED
+  where d = distDouble
 
 {-@ reflect bins' @-}
 {-@ bins' :: Prob -> Prob -> n:Double -> Distr Double @-}
@@ -163,6 +207,22 @@ seqBind u f x = bind u (f x)
 {-@ reflect flip @-}
 flip :: (a -> b -> c) -> b -> a -> c
 flip f x y = f y x
+
+{-@ flipPlus :: x:Double -> {(flip (pure2 plus) x) == (ppure . (plus x))} @-}
+flipPlus :: Double -> () 
+flipPlus x = extDouble (flip (pure2 plus) x) (ppure . (plus x)) (flipPlus' x)
+
+{-@ flipPlus' :: x:Double -> y:Double -> {(flip (pure2 plus) x y) == (ppure . (plus x)) (y)} @-}
+flipPlus' :: Double -> Double -> () 
+flipPlus' _ _ = ()
+
+{-@ assume extDouble :: f:(a -> b) -> g:(a -> b) 
+          -> (x:a -> {v:() | f x == g x}) -> {f == g } @-} 
+extDouble :: (a -> b) -> (a -> b) -> (a -> ()) -> () 
+extDouble _ _ _ = () 
+
+
+
 
 {-@ assume commutative :: e:Distr a -> u:Distr b -> f:(a -> b -> Distr c) 
                 -> {bind e (seqBind u f)
